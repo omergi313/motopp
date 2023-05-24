642f526a8597454741bb4c47ab3a6ad11aebda01
omergi313 <46580356+omergi313@users.noreply.github.com>
2023-05-24 22:18:53 +0300

    Update forms.py

--- motopp/forms.py
+++ motopp/forms.py
@@ -4,8 +4,16 @@ from wtforms.validators import DataRequired
 
 
 class AddBikeForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    make = TextAreaField('Make', validators=[DataRequired()])
-    model = TextAreaField('Model', validators=[DataRequired()])
-    year = IntegerField('Year', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    """
+    A FlaskForm subclass for the bike adding form.
+
+    :param name: The name of the bike.
+    :param make: The make of the bike.
+    :param model: The model of the bike.
+    :param year: The year of the bike.
+    """
+    name: StringField = StringField('Name', validators=[DataRequired()])
+    make: TextAreaField = TextAreaField('Make', validators=[DataRequired()])
+    model: TextAreaField = TextAreaField('Model', validators=[DataRequired()])
+    year: IntegerField = IntegerField('Year', validators=[DataRequired()])
+    submit: SubmitField = SubmitField('Submit')
